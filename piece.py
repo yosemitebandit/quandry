e28327a605e5373a78dfc526bf2de610738415ee
Matt Ball <matt.ball.2@gmail.com>
2015-09-29 20:37:08 -0700

    corner picking works for image a

--- piece.py
+++ piece.py
@@ -1,5 +1,6 @@
 """Puzzle piece image processing."""
 
+import itertools
 import math
 
 import matplotlib.pyplot as plt
@@ -9,8 +10,15 @@ from skimage import filters
 from skimage import io
 
 
+def distance(a, b):
+  """Get the distance between points a and b."""
+  dx = a[0] - b[0]
+  dy = a[1] - b[1]
+  return math.sqrt(dx**2 + dy**2)
+
+
 # Load the image.
-source = 'a.jpg'
+source = 'b.jpg'
 piece = io.imread(source, as_grey=True)
 
 # Find edges with the Canny filter.
@@ -23,7 +31,7 @@ canny_edges = np.logical_not(canny_edges)
 
 # Get corners.
 dist = 20
-sensitivity = 0.08
+sensitivity = 0.05
 window = 100
 harris = feature.corner_harris(canny_edges, k=sensitivity)
 coords = feature.corner_peaks(harris, min_distance=dist)
@@ -38,7 +46,70 @@ for coord in coords:
   x = coord[0] - center[0]
   y = coord[1] - center[1]
   angles.append(180 / math.pi * math.atan2(y, x))
-  #print coord, angles[-1]
+  print coord, angles[-1]
+
+print '\n'
+
+# Find corner sets.
+trig_threshold = 0.2
+center_dist_threshold = 0.15
+corner_sets = {}
+for i, coord_one in enumerate(coords):
+  corner_sets[i] = {
+    90: [],
+    180: [],
+  }
+  angle_one = math.pi / 180 * angles[i]
+  center_dist_one = distance(coord_one, center)
+  for k, coord_two in enumerate(coords):
+    if i == k:
+      continue
+    center_dist_two = distance(coord_two, center)
+    if (abs(center_dist_two - center_dist_one) / center_dist_one >
+        center_dist_threshold):
+      continue
+    angle_two = math.pi / 180 * angles[k]
+    cosine = math.cos(angle_one - angle_two)
+    sine = math.sin(angle_one - angle_two)
+    if abs(cosine) < trig_threshold:
+      corner_sets[i][90].append(k)
+    if abs(sine) < trig_threshold:
+      corner_sets[i][180].append(k)
+
+# Generate all possible rectangle candidates (collections of four corners).
+rect_candidates = []
+for index in corner_sets:
+  # Need at least two 90 neighbors and one 180 coord.
+  if len(corner_sets[index][90]) < 2:
+    continue
+  if len(corner_sets[index][180]) < 1:
+    continue
+  print index, coords[index], corner_sets[index][90], corner_sets[index][180]
+  print '\n'
+  # Choose the index, one 180 and two 90s to form a possible rectangle.
+  for one_eighty in corner_sets[index][180]:
+    for two_90s in itertools.combinations(corner_sets[index][90], 2):
+      rect_candidates.append([index, two_90s[0], one_eighty, two_90s[1]])
+
+# Get area of each rect candidate with Bretschneider's Formula.
+areas = []
+for rect in rect_candidates:
+  a = distance(coords[rect[0]], coords[rect[3]])
+  b = distance(coords[rect[1]], coords[rect[0]])
+  c = distance(coords[rect[2]], coords[rect[1]])
+  d = distance(coords[rect[3]], coords[rect[2]])
+  p = distance(coords[rect[0]], coords[rect[2]])
+  q = distance(coords[rect[1]], coords[rect[3]])
+  area = 0.25 * math.sqrt(4 * p**2 * q**2 - (b**2 + d**2 - a**2 - c**2)**2)
+  areas.append([rect, area])
+
+# Take a guess at the true corners by choosing the biggest area.
+sorted_areas = sorted(areas, key=lambda a: a[1], reverse=True)
+corner_coords = [[], []]
+for index in sorted_areas[0][0]:
+  corner_coords[0].append(coords[index][0])
+  corner_coords[1].append(coords[index][1])
+print corner_coords
 
 # Display results.
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(20, 8))
@@ -51,8 +122,9 @@ ax2.axis('off')
 ax2.set_title('Canny filter, $\sigma=%s$' % sigma, fontsize=20)
 
 ax3.imshow(canny_edges, cmap=plt.cm.gray)
-ax3.plot(coords[:, 1], coords[:, 0], '+r', markersize=12)
 ax3.plot(center[1], center[0], '*b', markersize=8)
+ax3.plot(corner_coords[1], corner_coords[0], 'og', markersize=12)
+ax3.plot(coords[:, 1], coords[:, 0], '+r', markersize=12)
 ax3.axis('off')
 ax3.set_title('Harris corners', fontsize=20)
 
