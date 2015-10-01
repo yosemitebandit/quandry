c98870d02452b1b54efad840a8374168fb98ac33
Matt Ball <matt.ball.2@gmail.com>
2015-10-01 16:51:12 -0700

    iteratively find a reasonable number of corner candidates

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 from quandry import JigsawPiece
 
 
-filepaths = ('a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg')
+#filepaths = ('a.jpg', 'b.jpg')#, 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg')
+filepaths = ('a.jpg',)
 subplot_rows = len(filepaths)
 plot_number = 0
 
@@ -16,6 +17,23 @@ for path in filepaths:
   print 'processing "%s"..' % path
   path = os.path.join('sample-pieces/', path)
   piece = JigsawPiece(path)
+
+  canny_sigma = 2
+  harris_sensitivity = 0.05
+  while True:
+    piece.find_edges(canny_sigma=canny_sigma)
+    piece.find_corners(harris_sensitivity=harris_sensitivity)
+    print '  %s candidate corners' % len(piece.candidate_corners)
+    if len(piece.candidate_corners) < 400:
+      break
+    canny_sigma += 0.1
+    harris_sensitivity += 0.01
+
+  piece.find_center()
+  piece.find_angles()
+  piece.find_corner_sets()
+  piece.find_rect_candidates()
+  piece.find_true_corners()
   # Plot the image.
   plot_number += 1
   plt.subplot(subplot_rows, 2, plot_number)
